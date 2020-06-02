5e5de660c5d3aaf29be69603cec369a49a718ec1
Marcell Kajati <marcell.kajati@gmail.com>
2020-06-02 17:16:18 +0200

    Added Damascus Poor District templars

--- ac1-collectibles/Damascus/damas_map.js
+++ ac1-collectibles/Damascus/damas_map.js
@@ -1,12 +1,16 @@
 let flagsFile;
+let templarsFile;
 
 function preload(){
     flagsFile = loadJSON('http://localhost/ac1-collectibles/Damascus/flags.json');
+    templarsFile = loadJSON('http://localhost/ac1-collectibles/Damascus/templars.json');
 }
 
 function setup() { 
     noCanvas();
+
     var bounds = [[0,0], [2303,4096]];
+    
     var map = L.map('damascusMap', {
         crs: L.CRS.Simple,
         minZoom: -1.2,
@@ -16,11 +20,21 @@ function setup() {
         maxBoundsViscosity: 1.0 
     });
 
+    var templarIcon = L.icon({
+        iconUrl: 'templar.png',
+        iconSize:     [35, 35],
+        iconAnchor:   [17.5, 17.5],
+        popupAnchor:  [0, 0]
+    });
+
     for(let flag of flagsFile.flags){
-        console.log(flag);
         L.marker([flag.y, flag.x]).bindPopup(flag.note).addTo(map);
     }
     
+    for(let templar of templarsFile.templars){
+        L.marker([templar.y, templar.x], {icon: templarIcon}).bindPopup(templar.note).addTo(map);
+    }
+
     L.imageOverlay('Damascus1.png', bounds).addTo(map);
     map.fitBounds(bounds);
 } 

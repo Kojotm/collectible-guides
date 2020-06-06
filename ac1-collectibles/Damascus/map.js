13e7083e6df5e7fcd1fbe5b60f915e316c4c4e99
Marcell Kajati <marcell.kajati@gmail.com>
2020-06-06 13:12:07 +0200

    Made visual changes.

--- ac1-collectibles/Damascus/map.js
+++ ac1-collectibles/Damascus/map.js
@@ -17,40 +17,41 @@ function setup() {
         maxZoom: 1,
         zoomSnap: 0,
         maxBounds: bounds,
-        maxBoundsViscosity: 1.0
+        maxBoundsViscosity: 1.0,
+        doubleClickZoom: false
     });
 
-    var templarIcon = L.divIcon({
-        html: '<img src="img/templar.png" width=35 height=35 class="markerIcon"/>',
+    var ActiveTemplarIcon = L.divIcon({
+        html: '<img src="img/Templar_Active.png" width=35 height=35 class="markerIcon"/>',
         iconAnchor:   [17.5, 17.5],
-        popupAnchor:  [0, 0],
+        tooltipAnchor:  [23, -8],
+        className: ''
+    });
+
+    var InactiveTemplarIcon = L.divIcon({
+        html: '<img src="img/Templar_Inactive.png" width=35 height=35 class="markerIcon"/>',
+        iconAnchor:   [17.5, 17.5],
+        tooltipAnchor:  [23, -8],
         className: ''
     });
 
     var ActiveFlagIcon = L.divIcon({
         html: '<img src="img/SaracenFlag_Active.png" width=30 height=50 class="markerIcon"/>',
         iconAnchor:   [15, 50],
-        popupAnchor:  [0, -50],
+        tooltipAnchor:  [20, -37.5],
         className: ''
     });
 
     var InactiveFlagIcon = L.divIcon({
         html: '<img src="img/SaracenFlag_Inactive.png" width=30 height=50 class="markerIcon"/>',
         iconAnchor:   [15, 50],
-        popupAnchor:  [0, -50],
+        tooltipAnchor:  [20, -37.5],
         className: ''
     });
 
     for(let flag of flagsFile.flags){
         var marker = L.marker([flag.y, flag.x], {icon: InactiveFlagIcon});
-        marker.bindPopup(flag.note, {closeButton: false})
-
-        marker.on('mouseover', function(ev) {
-            ev.target.openPopup();
-        });
-        marker.on('mouseout', function(ev) {
-            ev.target.closePopup();
-        });
+        marker.bindTooltip(flag.note);
         marker.on('click', function(ev){
             var layer = ev.target;
             layer.setIcon(layer.options.icon == InactiveFlagIcon ? ActiveFlagIcon : InactiveFlagIcon);
@@ -60,15 +61,14 @@ function setup() {
     }
     
     for(let templar of templarsFile.templars){
-        var marker = L.marker([templar.y, templar.x], {icon: templarIcon}).bindPopup(templar.note, {closeButton: false}).addTo(map);
+        var marker = L.marker([templar.y, templar.x], {icon: InactiveTemplarIcon});
+        marker.bindTooltip(templar.note);
+        marker.on('click', function(ev){
+            var layer = ev.target;
+            layer.setIcon(layer.options.icon == InactiveTemplarIcon ? ActiveTemplarIcon : InactiveTemplarIcon);
+        });
 
-        marker.on('mouseover', function(ev) {
-            ev.target.openPopup();
-        });
-        marker.on('mouseout', function(ev) {
-            ev.target.closePopup();
-        });
-        marker.off('click');
+        marker.addTo(map);
     }
 
     L.imageOverlay('img/map.png', bounds).addTo(map);
